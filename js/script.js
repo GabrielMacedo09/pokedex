41e299280ecfbd04617f050337a5b5408e85704e
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2023-03-01 19:19:20 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -30,17 +30,7 @@ const renderPokemon = async (pokemon) => {
   pokemonNumber.innerHTML = '';
 
   const data = await fetchPokemon(pokemon);
-  const mtg = require('mtgsdk');
 
-mtg.card.find(3)
-.then(result => {
-    console.log(result.card.name) // "Black Lotus"
-})
-
-mtg.set.find('AER')
-.then(result => {
-    console.log(result.set.name) // "Aether Revolt"
-})
 
   if (data) {
     pokemonImage.style.display = 'block';
